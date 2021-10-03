44f29ea47aada5b4c65e7257a8368e9ba16c4c22
kimanicharles911 <kimanicharles911@gmail.com>
2021-10-03 19:33:54 +0300

    Successful trial to change style of mine location now

---
+++ src/components/GridTableComponent.jsx
@@ -0,0 +1,66 @@
+/* I learnt to listen to different mouse buttons at: https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button */
+import { useState } from "react";
+import "./GridTableComponent.css";
+
+const GridTableComponent = () => {
+
+  const [gameStarted, setGameStarted] = useState(false)
+  const gridBoxesArr = [];
+  const mineLocationsArr = []; 
+  
+  const createGrid = () => {
+    let i = 1;
+    while(i <= 64){
+      gridBoxesArr.push(i);
+      i++;
+    }
+  };
+
+  const startGame = (divNum, eventTarget) => {
+    setGameStarted(true);
+    generateMineNumbers(eventTarget);
+  };
+
+  const generateMineNumbers = (eventTarget) => {
+    let i = 0;    
+    while(i < 11){
+      let randomNum = Math.floor(Math.random() * 64) + 1;
+    
+      if(mineLocationsArr.length === 0){
+        mineLocationsArr.push(randomNum);
+      }else if(mineLocationsArr.length > 0 && mineLocationsArr.length < 10){
+        if(!mineLocationsArr.includes(randomNum) && parseInt(eventTarget.attributes.custom_id.value) !== randomNum){
+          mineLocationsArr.push(randomNum);
+        }
+      }
+      i++;
+    }
+    setMineStyles(mineLocationsArr, eventTarget);
+  }
+
+  const setMineStyles = (mineLocationsArr, eventTarget) => {
+    for(const mineLocation of mineLocationsArr){
+      let sibling = eventTarget.parentNode.firstChild;
+      while(sibling !== null){
+        if(parseInt(sibling.attributes.custom_id.value) === mineLocation ){
+          sibling.className = "landmine-div";
+        }
+        sibling = sibling.nextSibling;
+      }      
+    }
+  };
+
+
+  createGrid();
+
+  return(
+    <section className="second-section">
+      {gridBoxesArr.map((gridBoxNum, index) => {
+        return <div className="un-clicked-div" custom_id={gridBoxNum} key={gridBoxNum} onClick={(event) => !gameStarted && startGame(gridBoxNum, event.target)}></div>
+      })}
+    </section>
+  );
+
+};
+
+export default GridTableComponent;
